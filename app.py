21e5f28244f238a4a1b7b69a54c08d43cbe3de72
Dushyant <98117205+MrD1405@users.noreply.github.com>
2026-08-06 23:19:57 +0530

    feat : Listening to entire request , Error handling

--- app.py
+++ app.py
@@ -20,15 +20,34 @@ server.bind(('127.0.0.1',8199))
 server.listen(1)
 print("server is listening")
 while True:
-    client_connection , client_address = server.accept()
-    request_stream = client_connection.recv(1024) 
-    request_data = parse_request_stream(request_stream.decode("utf-8"))   
-    #print(request_data)  
-    http_response=b"""\
-HTTP/1.1 200 OK
+    try:
+        client_connection , client_address = server.accept()
+        request_stream =b""
+        while b"\r\n\r\n" not in request_stream:
+            print("loop through first")
+            chunk = client_connection.recv(1024)
+            if not chunk :
+                break
+            request_stream+=chunk
+        http_structure , _ , body = request_stream.partition(b'\r\n\r\n')
+        http_method , request_target , headers = parse_http_headers(http_structure.decode("utf-8"))
+        expected_length = int(headers.get('content-length',0))
+        while expected_length > 0 :
+            print("loop thru second")
+            chunk = client_connection.recv(1024)
+            print(chunk)
+            if not chunk:
+                break
+            body+=request_stream
+            expected_length=expected_length-len(chunk)
+            print(f"expec len {expected_length}")
+        print("its here")
+        http_response=b"""\
+    HTTP/1.1 200 OK
 
-Hello World
-"""
-    client_connection.sendall(http_response)
-    client_connection.close()
-
+    Hello World
+    """
+        client_connection.sendall(http_response)
+        client_connection.close()
+    except Exception as e:
+        print(f"Bad Request : {e}")
